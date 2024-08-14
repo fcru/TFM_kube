2b2a12bfefd13883eefe34dd80d0ca49e9b37df7
fcru <f.cruanes@gmail.com>
2024-08-14 12:49:32 +0200

    Modified mongo-job.py to allow synchronous writing using write concern

--- pyspark-code/src/main/example/mongo-job.py
+++ pyspark-code/src/main/example/mongo-job.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymongo.write_concern import WriteConcern
 import uuid
 
 def main():
@@ -9,14 +10,14 @@ def main():
     database = mongo_client["testdb"]
 
     # Access the collection
-    collection = database["uuids"]
-
-    # Generate 10 random UUIDs
+    collection = database.get_collection('uuids', write_concern=WriteConcern(w=1))
     uuid_documents = [{"uuid": str(uuid.uuid4())} for _ in range(10)]
     print(uuid_documents)
     # Insert UUIDs into MongoDB collection
-    collection.insert_many(uuid_documents)
-    print("insertion completed")
+    result = collection.insert_many(uuid_documents)
+
+    print(f"Inserted {len(result.inserted_ids)} documents")
+
     # Close the MongoDB connection
     mongo_client.close()
 
